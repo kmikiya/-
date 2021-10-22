ffd812fb5cae7483b3a4560d7d298fe0304698b5
kmikiya <k.mikiya97@gmail.com>
2021-10-22 14:56:35 +0000

    1st

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
-  
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
    attachment :profile_image
    has_many :books, dependent: :destroy
    has_many :favorites, dependent: :destroy
    has_many :book_comments, dependent: :destroy
-   
+
    has_many :relationships, class_name: "Relationship", foreign_key: :follower_id
    #has_many :followeds, through: :relationships, source: :follower
    has_many :followings, through: :relationships, source: :followed
    has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id
   # has_many :followers, through: :reverse_of_relationships, source: :followed
    has_many :followers, through: :reverse_of_relationships, source: :follower
-   
+
      def follow(user_id)
-    relationships.create(followed_id: user_id)
-  end
+       relationships.create(followed_id: user_id)
+     end
 
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
@@ -30,8 +30,8 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
-   
+
    validates :name, presence: true, length: { in: 2..20 }
    validates :introduction, length: { maximum: 50 }
-   
+
 end
